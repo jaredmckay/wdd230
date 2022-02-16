e4323064317eacbcb68eca16ad723a4f2d69626e
jaredmckay <mck21022@byui.edu>
2022-02-15 19:54:00 -0600

    updates

--- chamber/js/popup.js
+++ chamber/js/popup.js
@@ -1,14 +1,12 @@
 
 const day = new Date();
 
-function popupDisplay() {
-    const weekDay = day.getDay();
-    const popup = document.getElementById("#popup");
-    if (weekDay === 1 || weekDay === 2) {
-        popup.style.display = "block"
-    }
-    else{
-        document.getElementById("#popup").style.visibility="hidden"
-        popup.style.display = "none";
-    }
+var weekDay = day.getDay();
+var popup = document.querySelector("#popup");
+if (weekDay === 1 | weekDay === 2) {
+    popup.style.display = "block"
+}
+else{
+    document.querySelector("#popup").style.visibility="hidden"
+    popup.style.display = "none";
 }
