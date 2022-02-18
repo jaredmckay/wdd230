22b409b8957997318c2285898c75731209bde37b
jaredmckay <mck21022@byui.edu>
2022-02-17 20:18:55 -0600

    lazyloac

--- lesson7/scripts/main.js
+++ lesson7/scripts/main.js
@@ -12,7 +12,7 @@ function preloadImage(img){
 
 const imgOptions = {
     threshold: 0,
-    rootMargin: "0px 0px 50px 0px"
+    rootMargin: "0px 0px -50px 0px"
 };
 
 const imgObserver = new IntersectionObserver((entries, imgObserver) => {
