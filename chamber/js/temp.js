3bce864a2771536f27be2dc5efc66a4b85db3b07
jaredmckay <mck21022@byui.edu>
2022-03-12 20:24:08 -0600

    weather

--- chamber/js/temp.js
+++ chamber/js/temp.js
@@ -1,17 +1,40 @@
-let temp = parseFloat(document.querySelector("#temp").textContent);
-let speed = parseFloat(document.querySelector("#speed").textContent);
-let windchill = "";
 
-if (temp <= 50 && speed > 3) {
-  windchill = windChill(temp, speed);
-  windchill = `${windchill}&#176;F`;
-} else {
-  windchill = "N/A";
-}
-// output
-document.querySelector("#windchill").innerHTML = windchill;
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=6113335&units=imperial&APPID=6a845f71ff0257569a77605dbb5fb42e";
 
-function windChill(temp, speed) {
-  var windChill = (35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16)).toFixed(0);
-  return windChill;
-}
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    let curTemp = jsObject.main.temp;
+    let curSpeed = jsObject.wind.speed;
+    let windchill = "";
+    document.querySelector('#current-temp').textContent = Math.round(curTemp);
+    document.querySelector('#current-speed').textContent = Math.round(curSpeed);
+    const iconsrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description;
+    
+  
+    
+
+
+
+
+    document.querySelector('#weathericon').setAttribute('src', iconsrc);
+    document.querySelector('#weathericon').setAttribute('alt', desc);
+    document.querySelector('figcaption').textContent = desc;
+  
+
+  if (curTemp <= 50 && curSpeed > 3) {
+    windchill = windChill(curTemp, curSpeed);
+    windchill = `${windchill}&#176;F`;
+  } else {
+    windchill = "N/A";
+  }
+  
+  document.querySelector("#windchill").innerHTML = windchill;
+
+  function windChill(curTemp, curSpeed) {
+    var windChill = (35.74 + 0.6215 * curTemp - 35.75 * Math.pow(curSpeed, 0.16) + 0.4275 * curTemp * Math.pow(curSpeed, 0.16)).toFixed(0);
+    return windChill;
+  }
+  });
